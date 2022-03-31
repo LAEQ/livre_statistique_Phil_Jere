6a4737a448e51b41b3f738b16cde8743858f7afe
Apparicio <Philippe.Apparicio@inrs.ca>
2022-03-30 21:05:02 -0400

    LIVRE FINAL

--- code_complementaire/COMPILE_BOOK.R
+++ code_complementaire/COMPILE_BOOK.R
@@ -1,25 +1,30 @@
 rm(list = ls())
 
+# Ne oublier de changer l'auteur avant de lancer le program "PA" ou "JG"
 author <- "PA"
 
 # DEFINITION DES CHEMINS VERS LES DEUX DOSSIERS PRINCIPAUX
-book_dir <- "C:/Users/GelbJ/Desktop/Projets/Philippe/__Livre_statistique/livre_statistique_Phil_Jere"
-website_dir <- "C:/Users/GelbJ/Desktop/Projets/Philippe/__Livre_statistique/WebSite/LivreStatistique_website"
+if(author == "PA"){
+  book_dir <- "D:/Articles et colloque/Livre en cours/AnalysesQuanti/Livre/livre_statistique_Phil_Jere"
+  website_dir <- "D:/Articles et colloque/Livre en cours/AnalysesQuanti/Livre/WebSite/LivreStatistique_website/LivreMethoQuantBolR"
+}else{
+ book_dir <- "C:/Users/GelbJ/Desktop/Projets/Philippe/__Livre_statistique/livre_statistique_Phil_Jere"
+ website_dir <- "C:/Users/GelbJ/Desktop/Projets/Philippe/__Livre_statistique/WebSite/LivreStatistique_website"
+}
 
 #------------------------------------------
 # COMPILATION DU LIVRE EN HTML ET PDF (UN PEU LONG)
 #------------------------------------------
 setwd(book_dir)
-bookdown::clean_book()
-rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
-rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+# bookdown::clean_book()
+# rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+# rmarkdown::render_site(output_format = 'bookdown::pdf_book', encoding = 'UTF-8')
 
 #------------------------------------------
 # Passer dans le dossier des fichiers html
 # Remplacer References par R?f?rences
-#  Cette partie  fonctionne!
+#  Cette partie fonctionne!
 #------------------------------------------
-
 dossier <- getwd()
 FilesHTML <- list.files(pattern = "*.html",recursive=TRUE)
 lapply(FilesHTML, function(e){
@@ -46,5 +51,5 @@ shell(paste0('cd "',website_dir,'" & git add -A & git commit -m "',message,'"'))
 #------------------------------------------
 # PUSH IT ONLINE
 #------------------------------------------
-shell(paste0('cd "',website_dir,'" git push origin'))
+shell(paste0('cd "',website_dir,'" & D: & git push origin'))
 
